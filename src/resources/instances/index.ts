38c9dbdf9a129b434b7c9246d321da6b6f9ed2d4
Rafael Garcia <raf@onkernel.com>
2025-12-23 03:34:08 +0000

    feat(api): add autogenerated stat endpoint from Stainless
    Cherry-picked from stainless/preview/feature/hypeman-cp
    
    - Add client.instances.stat() method
    - Add PathInfo response type
    - Add InstanceStatParams query parameters

--- src/resources/instances/index.ts
+++ src/resources/instances/index.ts
@@ -3,11 +3,13 @@
 export {
   Instances,
   type Instance,
+  type PathInfo,
   type PortMapping,
   type VolumeMount,
   type InstanceListResponse,
   type InstanceLogsResponse,
   type InstanceCreateParams,
   type InstanceLogsParams,
+  type InstanceStatParams,
 } from './instances';
 export { Volumes, type VolumeAttachParams, type VolumeDetachParams } from './volumes';
